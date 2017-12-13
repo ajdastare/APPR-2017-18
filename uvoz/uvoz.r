5cf051abf0a88b1509160baa84f9278f029ba79a
ajdastare <ajda157@gmail.com>
2017-12-13 13:29:55 +0100

    poravki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,51 +1,62 @@
 # 2. faza: Uvoz podatkov
+#1. CSV TABELA
+stanovanjska_p <- read.csv(file="stopnja_pre.csv",skip = 3, header=TRUE, sep=",")
+stanovanjska_p <- apply(stanovanjska_p,2, function(x) gsub(";","",x))
 
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
+
+#2.CSV TABELA 
+stopnja_p <- read.csv(file = "stopnja_prenaseljenosti.csv", header = TRUE, sep=",")
+
+stopnja_p <- apply(obsojeni,2, function(x) gsub("-",NA,x))
+
+# 
+# sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
+# 
+# # Funkcija, ki uvozi občine iz Wikipedije
+# uvozi.prenaseljenost_m <- function() {
+#   link <- "http://ec.europa.eu/eurostat/tgm/web/_download/Eurostat_Table_tessi170HTMLDesc_2ea2adcf-304b-4844-a32a-5d756878ea72.htm"
+#   stran <- html_session(link) %>% read_html()
+#   tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+#     .[[1]] %>% html_table(dec = ",")
+#   for (i in 1:ncol(tabela)) {
+#     if (is.character(tabela[[i]])) {
+#       Encoding(tabela[[i]]) <- "UTF-8"
+#     }
   }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
+  # colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+  #                       "ustanovitev", "pokrajina", "regija", "odcepitev")
+  # tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+  # tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+  # tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+  # for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+  #   tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
+  # }
+  # for (col in c("obcina", "pokrajina", "regija")) {
+  #   tabela[[col]] <- factor(tabela[[col]])
+  
   return(tabela)
 }
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
-}
+# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
+# uvozi.druzine <- function(obcine) {
+#   data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
+#                     locale = locale(encoding = "Windows-1250"))
+#   data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
+#     strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
+#   data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
+#   data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
+#                         value.name = "stevilo.druzin")
+#   data$velikost.druzine <- parse_number(data$velikost.druzine)
+#   data$obcina <- factor(data$obcina, levels = obcine)
+#   return(data)
+# }
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+# # Zapišimo podatke v razpredelnico obcine
+# obcine <- uvozi.obcine()
+# 
+# # Zapišimo podatke v razpredelnico druzine.
+# druzine <- uvozi.druzine(levels(obcine$obcina))
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
