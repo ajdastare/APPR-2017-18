739c36b57f1c51a7d2ea56c82387809973002e78
Ajda Stare <ajda157@gmail.com>
2018-01-03 12:54:56 +0100

    popravljen uvoz podatkov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,85 +1,75 @@
 # 2. faza: Uvoz podatkov
 #1. CSV TABELA
-stanovanjska_p <- read.csv2(file="stopnja_pre1.csv",skip = 3, header=FALSE, sep=";"
-                            
-                      )
+#STOPNJA PRENASELJENOSTI STANOVANJA (2005-2016)
+
+sl <- locale(encoding = "Windows-1250", decimal_mark = ".", grouping_mark = ",")
+datoteka1 <- "podatki/stopnja_pre1.csv"
+stopnja_prenaseljenosti <-read_delim(datoteka1, ";", skip = 4, n_max = 10, trim_ws = TRUE, locale = sl) %>%
+  fill(1:2) %>% drop_na(3)
+stan.pri <- read_delim(datoteka1, ";", skip = 4, n_max = 10, trim_ws = TRUE, locale = sl) %>%
+  fill(1:2) %>% drop_na(3)
+stolpci <- read_csv2(datoteka1, skip = 3, n_max = 1, col_names = FALSE,
+                     col_types = cols(.default = col_integer())) %>% t() %>%
+  cbind(data.frame(colnames(stan.pri) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
+  apply(1, paste, collapse = "")
+stolpci[1:3] <- c("element", "starost", "spol")
+colnames(stan.pri) <- stolpci
+stan.pri <- melt(stan.pri, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
+  mutate(stolpec = parse_character(stolpec)) %>%
+  transmute(leto = stolpec %>% strapplyc("^([0-9]+)") %>% unlist() %>% parse_number(),
+            status = stolpec %>% strapplyc("([^0-9]+)$") %>% unlist() %>% factor(),
+            element, starost, spol, stopnja)
 
 
-stanovanjska_p <- stanovanjska_p[-c(7:28),]
-stanovanjska_p <- stanovanjska_p[-c(2:3),-c(1:2)]
+#2.CSV TABELA -STANOVANJSKA PRIKRAJSANOST
+
+datoteka2 <- "podatki/stanovanjska_prikrajsanost.csv"
+stan.pri <- read_delim(datoteka2, ";", skip = 4, n_max = 68, trim_ws = TRUE, locale = sl) %>%
+  fill(1:2) %>% drop_na(3)
+stolpci <- read_csv2(datoteka2, skip = 3, n_max = 1, col_names = FALSE,
+                     col_types = cols(.default = col_integer())) %>% t() %>%
+  cbind(data.frame(colnames(stan.pri) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
+  apply(1, paste, collapse = "")
+stolpci[1:3] <- c("element", "starost", "spol")
+colnames(stan.pri) <- stolpci
+stan.pri <- melt(stan.pri, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
+  mutate(stolpec = parse_character(stolpec)) %>%
+  transmute(leto = stolpec %>% strapplyc("^([0-9]+)") %>% unlist() %>% parse_number(),
+            status = stolpec %>% strapplyc("([^0-9]+)$") %>% unlist() %>% factor(),
+            element, starost, spol, stopnja)
 
 
-vektor_z_letnicami <- stanovanjska_p[1, ]
-ime_vrstic <- c("leto","Moški","Ženske","NULL")
-
-rownames(stanovanjska_p)<-ime_vrstic
-stanovanjska_p <- stanovanjska_p[,-c(1)]
-stanovanjska_p <- stanovanjska_p[-c(4),]
-colnames(stanovanjska_p)<-c(2005:2016)
-stanovanjska_p <- stanovanjska_p[-c(1),]
-
-View(stanovanjska_p)
-
-
-#2.CSV TABELA 
-stopnja_p <- read.csv2(file = "stopnja_prenaseljenosti.csv", header = FALSE, sep=";")
-vrstice <- stopnja_p[3,]
-colnames(stopnja_p) <- vrstice
-
-
+# uvozi.stanovanjska_prikrajsanost <- function(){
+# stanovanjska_prikrajsanost <- read.csv2(file = "stanovanjska_prikrajsanost.csv", header = FALSE, sep=";")
+# stanovanjska_prikrajsanost <- stanovanjska_prikrajsanost[-c(73:98),]
+# stanovanjska_prikrajsanost<- stanovanjska_prikrajsanost[-c(1:2),]
+# View(stanovanjska_prikrajsanost)
+# 
+# }
+# 
 
 # stopnja_p <- apply(obsojeni,2, function(x) gsub("-",NA,x))
 
 # 
 # sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
-# 
-# # Funkcija, ki uvozi občine iz Wikipedije
-# uvozi.prenaseljenost_m <- function() {
-#   link <- "http://ec.europa.eu/eurostat/tgm/web/_download/Eurostat_Table_tessi170HTMLDesc_2ea2adcf-304b-4844-a32a-5d756878ea72.htm"
-#   stran <- html_session(link) %>% read_html()
-#   tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-#     .[[1]] %>% html_table(dec = ",")
-#   for (i in 1:ncol(tabela)) {
-#     if (is.character(tabela[[i]])) {
-#       Encoding(tabela[[i]]) <- "UTF-8"
-#     }
-  
-  # colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-  #                       "ustanovitev", "pokrajina", "regija", "odcepitev")
-  # tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  # tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  # tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  # for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-  #   tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  # }
-  # for (col in c("obcina", "pokrajina", "regija")) {
-  #   tabela[[col]] <- factor(tabela[[col]])
-#   
-#   return(tabela)
-# }
 
-# # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-# uvozi.druzine <- function(obcine) {
-#   data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-#                     locale = locale(encoding = "Windows-1250"))
-#   data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-#     strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-#   data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-#   data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-#                         value.name = "stevilo.druzin")
-#   data$velikost.druzine <- parse_number(data$velikost.druzine)
-#   data$obcina <- factor(data$obcina, levels = obcine)
-#   return(data)
-# }
 
-# # Zapišimo podatke v razpredelnico obcine
-# obcine <- uvozi.obcine()
-# 
-# # Zapišimo podatke v razpredelnico druzine.
-# druzine <- uvozi.druzine(levels(obcine$obcina))
+# 3.HTML TABELA
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+
+pre.moski <- read_html("podatki/prenaseljenost_eurostat_moski.htm") %>%
+  html_node(xpath="//table[@class='infoData']") %>% html_table() %>%
+  melt(value.name = "stopnja", id.vars = "timegeo", variable.name = "leto")
+
+
+pre.zenske <- read_html("podatki/prenaseljenost_eurostat_zenske.htm") %>%
+  html_node(xpath="//table[@class='infoData']") %>% html_table() %>%
+  melt(value.name = "stopnja", id.vars = "timegeo", variable.name = "leto")
+
+spoli <- c("moski", "zenske")
+pre.moski$spol <- factor("moski", levels = spoli)
+pre.zenske$spol <- factor("zenske", levels = spoli)
+
+prenaseljenost <- rbind(pre.moski, pre.zenske) %>% mutate(stopnja = parse_number(stopnja, na = ":"))
+
+
