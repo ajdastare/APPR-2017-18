7591c8f9cd71428e5b1c8a3366ceb0e7b01b9ae8
Ajda Stare <ajda157@gmail.com>
2018-01-04 09:53:45 +0100

    Dokončan uvoz podatkov.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -4,40 +4,53 @@
 
 sl <- locale(encoding = "Windows-1250", decimal_mark = ".", grouping_mark = ",")
 datoteka1 <- "podatki/stopnja_pre1.csv"
-stopnja_prenaseljenosti <-read_delim(datoteka1, ";", skip = 4, n_max = 10, trim_ws = TRUE, locale = sl) %>%
+
+stopnja_prenaseljenosti <-read_delim(datoteka1, ";",skip = 3, trim_ws = TRUE, locale = sl) %>%
   fill(1:2) %>% drop_na(3)
-stan.pri <- read_delim(datoteka1, ";", skip = 4, n_max = 10, trim_ws = TRUE, locale = sl) %>%
-  fill(1:2) %>% drop_na(3)
-stolpci <- read_csv2(datoteka1, skip = 3, n_max = 1, col_names = FALSE,
-                     col_types = cols(.default = col_integer())) %>% t() %>%
-  cbind(data.frame(colnames(stan.pri) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
-  apply(1, paste, collapse = "")
-stolpci[1:3] <- c("element", "starost", "spol")
-colnames(stan.pri) <- stolpci
-stan.pri <- melt(stan.pri, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
-  mutate(stolpec = parse_character(stolpec)) %>%
-  transmute(leto = stolpec %>% strapplyc("^([0-9]+)") %>% unlist() %>% parse_number(),
-            status = stolpec %>% strapplyc("([^0-9]+)$") %>% unlist() %>% factor(),
-            element, starost, spol, stopnja)
+
+# col_names(stopnja_prenaseljenosti)= c("Stopnja prenaseljenosti stanovanja","Vsa gospodinjstva","Spol",2005:2016)
+
+tab2 <- read_csv2(file=datoteka1,skip = 6, n_max= 2, 
+                    col_names = c("Stopnja prenaseljenosti stanovanja","Vsa gospodinjstva","Spol",2005:2016),
+                    locale=locale(encoding="Windows-1250"))
+  
+
+
+
+# stan.pri <- read_delim(datoteka1, ";", skip = 3,  trim_ws = TRUE, locale = sl) %>%
+#   fill(1:2) %>% drop_na(3)
+# stolpci1 <- read_csv2(datoteka1, skip = 3, n_max = 1, col_names = FALSE,
+#                      col_types = cols(.default = col_integer())) %>% t() %>%
+#   cbind(data.frame(colnames(stopnja_prenaseljenosti) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
+#   apply(1, paste, collapse = "")
+# stolpci1[1:3] <- c("element", "starost", "spol")
+# colnames(stan.pri) <- stolpci1
+# stan.pri <- melt(stan.pri, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
+  # mutate(stolpec = parse_character(stolpec)) %>%
+  # transmute(leto = stolpec %>% strapplyc("^([0-9]+)") %>% unlist() %>% parse_number(),
+            # status = stolpec %>% strapplyc("([^0-9]+)$") %>% unlist() %>% factor(),
+            # element, starost, spol, stopnja)
 
 
 #2.CSV TABELA -STANOVANJSKA PRIKRAJSANOST
 
-datoteka2 <- "podatki/stanovanjska_prikrajsanost.csv"
-stan.pri <- read_delim(datoteka2, ";", skip = 4, n_max = 68, trim_ws = TRUE, locale = sl) %>%
+#datoteka2 <- "podatki/stanovanjska_prikrajsanost.csv"
+datoteka2 <- "podatki/0867806Ss.csv"
+stan.pri2 <- read_delim(datoteka2, ";", skip = 4, n_max = 68, trim_ws = TRUE, locale=sl) %>%
   fill(1:2) %>% drop_na(3)
-stolpci <- read_csv2(datoteka2, skip = 3, n_max = 1, col_names = FALSE,
+stolpci2 <- read_csv2(datoteka2, skip = 3, n_max = 1, col_names = FALSE,
                      col_types = cols(.default = col_integer())) %>% t() %>%
-  cbind(data.frame(colnames(stan.pri) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
+  cbind(data.frame(colnames(stan.pri2) %>% strapplyc("^([^_]+)") %>% unlist())) %>% fill(1) %>%
   apply(1, paste, collapse = "")
-stolpci[1:3] <- c("element", "starost", "spol")
-colnames(stan.pri) <- stolpci
-stan.pri <- melt(stan.pri, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
-  mutate(stolpec = parse_character(stolpec)) %>%
+stolpci2[1:3] <- c("element", "starost", "spol")
+colnames(stan.pri2) <- stolpci2
+stan.pri2.tidy <- melt(stan.pri2, value.name = "stopnja", id.vars = 1:3, variable.name = "stolpec") %>%
+  mutate(stolpec = parse_character(stolpec, locale = sl)) %>%
   transmute(leto = stolpec %>% strapplyc("^([0-9]+)") %>% unlist() %>% parse_number(),
             status = stolpec %>% strapplyc("([^0-9]+)$") %>% unlist() %>% factor(),
             element, starost, spol, stopnja)
 
+tabela1 <- stan.pri2
 
 # uvozi.stanovanjska_prikrajsanost <- function(){
 # stanovanjska_prikrajsanost <- read.csv2(file = "stanovanjska_prikrajsanost.csv", header = FALSE, sep=";")
