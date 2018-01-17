04d42f8d7d6bf6a8756ba70dcb58c1c1aea79254
Ajda Stare <ajda157@gmail.com>
2018-01-17 09:57:39 +0100

    Dokončan zemljevid

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -16,22 +16,36 @@ graf1 <- ggplot(data =tabela2, aes(x =Leto, y =(odstotek_oseb),color = Spol ))+
 # Uvozimo zemljevid.
 svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
                           "ne_50m_admin_0_countries", encoding = "UTF-8") %>%
-  pretvori.zemljevid() %>% filter(lat > -60)
+  pretvori.zemljevid() %>%  filter(CONTINENT %in% c("Europe"))
+  
+  # filter(lat > -60)
+
+zemljevid_drzav <-ggplot() + geom_polygon(data = prenaseljenost %>% 
+                                      mutate(SOVEREIGNT = parse_factor(timegeo,levels(svet$SOVEREIGNT)))%>%
+                                      right_join(svet, by = c("timegeo" = "NAME_LONG")),
+                                    aes(x= long, y = lat,
+                                        group = group,
+                                        fill = stopnja)) +
+  coord_cartesian(xlim = c(-22, 40), ylim = c(30, 70)) +
+  ggtitle("Stopnja prenaseljenosti v Evropi")
+
+print(zemljevid_drzav)
 
 
-svet1 <- ggplot()+geom_polygon(data=svet, aes(x=long, y= lat, group = group))
-print(svet1)
 
-#rada bi dala podatke iz tabele : prenaseljenosti v zemljeviud
+# svet1 <- ggplot()+geom_polygon(data=svet, aes(x=long, y= lat, group = group))
+# print(svet1)
+
+#rada bi dala podatke iz tabele prenaseljenosti v zemljeviud
 
 prenaseljenost_moski <- prenaseljenost %>% filter(spol == "moski", stopnja != "NA")
 
 prenaseljenost_zenske <- prenaseljenost %>% filter(spol == "zenske", stopnja != "NA")
 
 #odstranla podatke z vrednostjo NA 
-zemljevid2 <-ggplot() + aes(x = long, y=lat, group= group, fill= stopnja ) + geom_polygon(data = svet %>% filter(CONTINENT == "Europe" |
-                                                                                                                   SOVEREIGNT %in% c("Canada",
-                                                                                                                                     "United States of America")))
+# zemljevid2 <-ggplot() + aes(x = long, y=lat, group= group, fill= stopnja ) + geom_polygon(data = svet %>% filter(CONTINENT == "Europe" |
+#                                                                                                                    SOVEREIGNT %in% c("Canada",
+#                                                                                                                                      "United States of America")))
 print(zemljevid2)
 
 
