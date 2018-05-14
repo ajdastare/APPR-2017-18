6567ec0d9c4e42ff02d901a9bf8649c361cec56a
ajdastare <ajda157@gmail.com>
2018-05-14 08:19:21 +0200

    Vizualizacija

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -5,76 +5,65 @@ library(dplyr)
 library(digest)
 library(maptools)
 
-# 1.Graf bo prikazoval stopnjo prenaseljenosti v Sloveniji(STOPNJA PRENASELJENOSTI STANOVANJA (2005-2016))
-
+#1.Graf bo prikazoval stopnjo prenaseljenosti v Sloveniji(STOPNJA PRENASELJENOSTI STANOVANJA (2005-2016))
 graf1 <- ggplot(data =tabela2, aes(x =Leto, y =(odstotek_oseb),colour = Spol ))+
   geom_point(shape=1)+
   geom_smooth(method=lm , color="red", se=TRUE)+
   xlab("Leto") + ylab("Odstotek oseb") +
   ggtitle("Stopnja prenaseljenosti stanovanja (2005-2016)")
 
-# Graf : stanovanjske prikrajsanosti 
-
+#2.Graf : stanovanjske prikrajsanosti 
+naslov_graf2 <- "Stanovanjske prikrajšanosti"
+Encoding(naslov_graf2)<- "UTF-8"
+oznake <- c("Slabo stanje stanovanja" = "Slabo stanje\nstanovanja",
+            "Kad ali prha v stanovanju" = "Kad ali prha\nv stanovanju",
+            "Stranišče na izplakovanje za lastno uporabo" = "Stranišče na\nizplakovanje za\nlastno uporabo",
+            "Pretemno stanovanje" = "Pretemno stanovanje")
+Encoding(oznake) <- "UTF-8"
+Encoding(names(oznake)) <- "UTF-8"
 status.skupaj <- "Status tveganja revščine -SKUPAJ"
 Encoding(status.skupaj) <- "UTF-8"
+
 graf2 <- ggplot(data = tabela1 %>%
                   filter(starost == "Starostne skupine - SKUPAJ",
                          status == status.skupaj,
                          spol != "Spol - SKUPAJ"),
-                aes(x = leto, y = stopnja, fill = spol)) + 
+                aes(x = leto, y = stopnja, fill = spol))+
   geom_col(position = "dodge") +
-  facet_grid(. ~ element)
+  facet_grid(. ~ oznake[element], space = "free")+
+  ggtitle(naslov_graf2)
 
-# graf2 <- ggplot(data = tabela1 %>% filter(starost== "Starostne skupine - SKUPAJ",status == "Status tveganja revščine -SKUPAJ",spol != "Spol - SKUPAJ"), aes(x= leto, y = stopnja,fill= spol)) + 
-#   geom_col()+
-#   facet_grid( .~ element)
-#   # facet_grid(paste(element, "~ ."))
-# 
-# oznake <- c("Slabo stanje stanovanja" = "Slabo stanje\nstanovanja",
-#             "Kad ali prha v stanovanju" = "Kad ali prha\nv stanovanju",
-#             "Stranišče na izplakovanje za lastno uporabo" = "Stranišče na\nizplakovanje za\nlastno uporabo",
-#             "Pretemno stanovanje" = "Pretemno stanovanje")
-# Encoding(oznake) <- "UTF-8"
-# Encoding(names(oznake)) <- "UTF-8"
-# graf22 <- ggplot(data = tabela1 %>% filter(status == "Status tveganja revščine -SKUPAJ",
-#                                           starost == "Starostne skupine - SKUPAJ",
-#                                           spol != "Spol - SKUPAJ"),
-#                 aes(x= leto, y = stopnja, fill = spol)) + 
-#   geom_col(position = "dodge") +
-#   facet_grid( ~ oznake[element])
-
-# Graf: samoocene splosnega zadovoljstva življenja
-
+#3.Graf: samoocene splosnega zadovoljstva življenja
+naslov_graf3 <- "Samoocena splošnega zadovoljstva življenja"
+Encoding(naslov_graf3)<-"UTF-8"
 graf3 <- ggplot(data = tabela_zadovoljstvo, aes(x=leto, y = odstotek, colour = ocena ))+ 
   geom_line()+
   xlab("Leto") + ylab("Odstotek") +
-  ggtitle("Samoocena splošnega zadovoljstva življenja")
+  ggtitle(naslov_graf3)
 
 
-# Graf : Breme stanovanjskih stroškov
-
-# graf4 <- ggplot(data = breme_stanovanjskih_stroskov, aes(x = leto, y= odstotek, color = velikost.bremena))+
-#   geom_line()+
-#   facet_grid(. ~ gospodinjstvo)+
-#   xlab("Leto") + ylab("Odstotek")+
-#   ggtitle("Breme stanovanjskih stroskov") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-# graf4$labels$colour <- "Velikost bremena"
+#4.Graf : Breme stanovanjskih stroškov
+naslov_graf4 <- "Breme stanovanjskih stroškov"
+Encoding(naslov_graf4)<- "UTF-8"
 graf4 <- ggplot(data = breme_stanovanjskih_stroskov,
                 aes(x = leto, y = odstotek, color = velikost.bremena)) +
   geom_line() + facet_grid(. ~ gospodinjstvo) + xlab("Leto") + ylab("Odstotek") +
-  ggtitle("Breme stanovanjskih stroškov") + guides(color = guide_legend("Velikost bremena")) +
+  ggtitle(naslov_graf4) + guides(color = guide_legend("Velikost bremena")) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-# Breme stanovanjskih stroškov - Prebivalci, ki jim stanovanjski stroški predstavljajo preveliko breme
+
+#4.2 Graf: delež prebivalcev katerim stanovanjski stroški predstavljajo preveliko breme:
+naslov_graf42 <- "Delež prebivalcev katerim stanovanjski stroški predstavljajo preveliko breme"
+Encoding(naslov_graf42)<- "UTF-8"
+graf42 <- ggplot(data= delez, aes(x =leto, y = stopnja))+
+  geom_line()+
+  facet_grid(. ~ drzava)+
+  ggtitle(naslov_graf42)
 
 
-# graf4 <- ggplot(data = breme_stanovanjskih_stroskov, aes(x = leto, y= odstotek, color=velikost.bremena) )+
-#   geom_col() +
-#   facet_grid(. ~ gospodinjstvo)+
-#   xlab("Leto")+ ylab("Odstotek") +
-#   ggtitle("Breme stanovanjskih stroškov")+
-#   guides(color = guide_legend("Velikost bremena"))
-
-# Graf: Stopnje prenaseljenosti v eu 
+graf123 <- ggplot(data = delez %>% filter(drzava == "Slovenia"),
+                  aes(x = leto, y = stopnja))+
+  geom_point() 
+#5.Graf: Stopnje prenaseljenosti v eu 
 
 graf5 <- ggplot(data= prenaseljenost, aes(x = timegeo, y = stopnja))+
   geom_col() + facet_grid(. ~ leto)
@@ -82,47 +71,35 @@ graf5 <- ggplot(data= prenaseljenost, aes(x = timegeo, y = stopnja))+
 
 # Uvozimo zemljevid.
 svet <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
-                          "ne_50m_admin_0_countries", encoding = "UTF-8") %>%
+                        "ne_50m_admin_0_countries", encoding = "UTF-8") %>%
   pretvori.zemljevid() %>%  filter(CONTINENT %in% c("Europe"))
-  
 
+#NASLOVA
 naslov <- "Stopnja prenaseljenosti za moške v Evropi"
 Encoding(naslov) <- "UTF-8"
-zemljevid_moski <-ggplot() + geom_polygon(data = prenaseljenost%>% filter(spol == "moski") %>% 
-                                      mutate(SOVEREIGNT = parse_factor(timegeo,levels(svet$SOVEREIGNT)))%>%
-                                      right_join(svet, by = c("timegeo" = "NAME_LONG")),
-                                    aes(x= long, y = lat,
-                                        group = group,
-                                        fill = stopnja)) +
-  coord_cartesian(xlim = c(-22, 40), ylim = c(30, 70)) +
-  ggtitle(naslov)
-zemljevid_zenske <-ggplot() + geom_polygon(data = prenaseljenost%>% filter(spol == "zenske") %>% 
+naslov2 <- "Stopnja prenaseljenosti za ženske v Evropi"
+Encoding(naslov2)<- "UTF-8"
+#ZEMLJEVID: Stopnja prenaseljenosti za moske
+zemljevid_moski <-ggplot() + geom_polygon(data = prenaseljenost%>% filter(spol == "moski", leto == "2016") %>% 
                                             mutate(SOVEREIGNT = parse_factor(timegeo,levels(svet$SOVEREIGNT)))%>%
                                             right_join(svet, by = c("timegeo" = "NAME_LONG")),
                                           aes(x= long, y = lat,
                                               group = group,
                                               fill = stopnja)) +
   coord_cartesian(xlim = c(-22, 40), ylim = c(30, 70)) +
-  ggtitle("Stopnja prenaseljenosti za ženske v Evropi")
+  ggtitle(naslov)
+# ZEMLJEVID: Stopnja prenaseljenosti za ženske
+
+zemljevid_zenske <-ggplot() + geom_polygon(data = prenaseljenost%>% filter(spol == "zenske", leto == "2016") %>% 
+                                             mutate(SOVEREIGNT = parse_factor(timegeo,levels(svet$SOVEREIGNT)))%>%
+                                             right_join(svet, by = c("timegeo" = "NAME_LONG")),
+                                           aes(x= long, y = lat,
+                                               group = group,
+                                               fill = stopnja)) +
+  coord_cartesian(xlim = c(-22, 40), ylim = c(30, 70)) +
+  ggtitle(naslov2)
 
 
 
-
-
-# 
-# # svet1 <- ggplot()+geom_polygon(data=svet, aes(x=long, y= lat, group = group))
-# # print(svet1)
-# 
-# #rada bi dala podatke iz tabele prenaseljenosti v zemljeviud
-# 
-# prenaseljenost_moski <- prenaseljenost %>% filter(spol == "moski", stopnja != "NA")
-# 
-# prenaseljenost_zenske <- prenaseljenost %>% filter(spol == "zenske", stopnja != "NA")
-# 
-# #odstranla podatke z vrednostjo NA 
-# # zemljevid2 <-ggplot() + aes(x = long, y=lat, group= group, fill= stopnja ) + geom_polygon(data = svet %>% filter(CONTINENT == "Europe" |
-# #                                                                                                                    SOVEREIGNT %in% c("Canada",
-# #                                                                                                                                      "United States of America")))
-# print(zemljevid2)
 
 
